4731a686bd9ab3acb87f6971d67fee1bb73ce84f
Rene Sanger <renejsanger@gmail.com>
2017-03-02 06:46:19 -0500

    pushed tdd

---
+++ TDD/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "prob.h"
+#include "basicmath.h"
+
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+  cout << fact(5) << endl;
+  cout << divide(10,2) << endl;
+  return 0;
+}
---
+++ TDD/prob.cpp
@@ -0,0 +1,6 @@
+#include "prob.h"
+
+int fact(int n){
+
+  return 0;
+}
